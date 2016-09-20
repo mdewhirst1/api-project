b3515ea4aa9a43ba08cfbe92a186e26ca4bd174e
Matthew Dewhirst <matthew.dewhirst@skybettingandgaming.com>
2016-09-20 14:16:57 +0100

    created navigation partial

--- config/routes.js
+++ config/routes.js
@@ -5,20 +5,20 @@ var bookController = require('../controllers/book-controller.js');
 
 // add routes here
 router.route("/")
-	.get(bookController.index)
-	.post(bookController.create);
+  .get(bookController.index)
+  .post(bookController.create);
 
 // New
 router.route("/new")
-	.get(bookController.new);
+  .get(bookController.new);
 
 router.route("/:id")
-	.get(bookController.show)
-	.put(bookController.update)
-	.delete(bookController.delete);
+  .get(bookController.show)
+  .put(bookController.update)
+  .delete(bookController.delete);
 
 // Edit
 router.route("/:id/edit")
-	.get(bookController.edit);
+  .get(bookController.edit);
 
 module.exports = router;
