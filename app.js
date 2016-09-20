6b6e0060dbe5146e0c6adf46a4ef265ffd0aec93
Matthew Dewhirst <matthew.dewhirst@skybettingandgaming.com>
2016-09-20 16:44:14 +0100

    new book stuff working

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var ejs = require('ejs');
 var layouts = require('express-ejs-layouts');
 var app = express();
 
+var bodyParser = require('body-parser');
+
 var bookRoutes = require('./config/books-routes');
 var gameRoutes = require('./config/games-routes');
 
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(layouts);
 
