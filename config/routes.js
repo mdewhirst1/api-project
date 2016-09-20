6980265f1bf04976d68a16db56b19abc33d2d0cf
Matthew Dewhirst <matthew.dewhirst@skybettingandgaming.com>
2016-09-20 14:26:39 +0100

    routes updated

--- config/routes.js
+++ config/routes.js
@@ -4,21 +4,21 @@ var router = express.Router();
 var bookController = require('../controllers/book-controller.js');
 
 // add routes here
-router.route("/")
+router.route("/books")
   .get(bookController.index)
   .post(bookController.create);
 
 // New
-router.route("/new")
+router.route("/books/new")
   .get(bookController.new);
 
-router.route("/:id")
+router.route("/books/:id")
   .get(bookController.show)
   .put(bookController.update)
   .delete(bookController.delete);
 
 // Edit
-router.route("/:id/edit")
+router.route("/books/:id/edit")
   .get(bookController.edit);
 
 module.exports = router;
