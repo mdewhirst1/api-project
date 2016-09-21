605bb54ccfc2f460d366abe1059f531c93b3cd57
Laura Salivonaite <laura.salivonaite@skybettingandgaming.com>
2016-09-21 13:45:25 +0100

    game controller edit New/Update/Delete/Edit

--- controllers/game-controller.js
+++ controllers/game-controller.js
@@ -1,24 +1,4 @@
-var games=[{
-    id: 0,
-    title: "Dead by Dayligh",
-    body:  "Run from the villain and fix generators, ESCAPE!"
-  },
-  {
-    id: 1,
-    title: "League of Legends",
-    body:  "Salty and rude players"
-  },
-  {
-    id: 2,
-    title: "Fallout 4",
-    body:  "Apocalyptic story game, RADIATION!"
-  },
-  {
-    id: 3,
-    title: "FIFA",
-    body:  "The most boring game ever invented"
-}];
-
+var Game = require("../models/game");
 // INDEX
 function indexGame(req, res) {
   res.render("games/index", {
@@ -50,7 +30,7 @@ function createGame(req, res) {
 
 // NEW
 function newGame(req, res) {
-  var game= {
+  var newGame= {
     id: "",
     title: "",
     body: ""
@@ -58,34 +38,37 @@ function newGame(req, res) {
 
   res.render("games/new" , {
     title: "New Game",
-    game: game,
+    game: newGame,
     edit: false
   });
 }
 
 // UPDATE
 function updateGame(req, res) {
-  var game= games[req.params.id];
-    game.title=req.body.title;
-    game.body= req.body.body;
-    games[req.params.id]= game;
-
+  Game.findByIdAndUpdate(req.params.id,{ $set: req.body}, function(err, game){
     res.redirect("/games");
+  });
 }
 
 // DELETE
 function deleteGame(req, res) {
-  games.splice(req.params.id, 1);
-  res.redirect("/games");
+  Game.findByIdAndRemove( req.params.id, function(err){
+    res.redirect("/games");
+  });
 }
 
 // EDIT
 function editGame(req, res) {
-  res.render("games/edit" , {
-  title: "Edit Game",
-  game: games[req.params.id],
-  edit: true
-});
+  Game.findById(req.params.id , function(err, game) {
+      // check for errors or for no object found
+      if(!game) return res.status(404).send("Not found");
+      if(err) return res.status(500).send(err);
+
+      res.render("games/edit" , {
+        title: "Game",
+        game: game
+      });
+  });
 }
 
 module.exports = {
